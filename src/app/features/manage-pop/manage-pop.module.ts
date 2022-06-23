1b17a81bb4bec97411303ef53c426666d408288a
federicodecaro <federicodcr96@outlook.it>
2022-06-23 17:57:29 +0200

    padding commit

--- src/app/features/manage-pop/manage-pop.module.ts
+++ src/app/features/manage-pop/manage-pop.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ManagePopComponent } from './manage-pop.component';
 import {RouterModule, Routes} from "@angular/router";
 import {PipesModule} from "../../shared/pipes/pipes.module";
-import {AppRoutingModule} from "../../app-routing.module";
 
 const routes : Routes = [
   {path: '', component: ManagePopComponent},
