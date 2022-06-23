86e49019fc8eb446d39829de080a673acf462e72
DESKTOP-GO6P2KJ\EmilianoUnikey <luca.papaleorm@gmail.com>
2022-06-23 17:47:31 +0200

    "The End"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", loadChildren: () => import("./features/home/home.module").then(m => m.HomeModule)},
+  {path:"detailsPop", loadChildren: () => import("./features/details-pop/details-pop.module").then(m => m.DetailsPopModule)},
+  {path:"gestionePops", loadChildren: () => import("./features/gestione-pops/gestione-pops.module").then(m => m.GestionePopsModule)},
+  {path:"preferedList", loadChildren: () => import("./features/prefered-list/prefered-list.module").then(m => m.PreferedListModule)},
+  {path:"**", redirectTo:"home",pathMatch:"full"},
+  {path:"", redirectTo:"home",pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderModule} from "./core/header/header.module";
+import {FooterModule} from "./core/footer/footer.module";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HeaderModule,
+        FooterModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
